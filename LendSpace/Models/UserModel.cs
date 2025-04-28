eb73bb8b478a6066bc88b9bec4ec5f45a4247710
Ian Dal <ianjohndal5@gmail.com>
2025-04-29 01:17:48 +0800

    added profile page, working profile picture

--- LendSpace/Models/UserModel.cs
+++ LendSpace/Models/UserModel.cs
@@ -1,22 +1,21 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
-
 namespace LendSpace.Models
 {
     public class UserModel : IdentityUser
     {
         [Required]
         public string? FirstName { get; set; }
-
         [Required]
         public string? LastName { get; set; }
-
         [Required]
         [Display(Name = "Middle Initial")]
         public string? MidInitial { get; set; }
-
         [Required]
         public string? Address { get; set; }
+
+        // Add this new property
+        public string? ProfilePictureUrl { get; set; }
 
         [Required]
         public ICollection<BillingModel> Billings { get; } = new List<BillingModel>();
